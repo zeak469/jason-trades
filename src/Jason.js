b63fa965c8212bf5d7bbf98be3788e9d8a3d8668
Ubuntu <ubuntu@ip-172-31-7-169.us-west-1.compute.internal>
2020-09-22 07:06:47 +0000

    Finishing front end touches.

--- src/Jason.js
+++ src/Jason.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 function Jason(){
     return (
-        <div
-        style={{
-            position: 'absolute', left: '50%', top: '10%',
-        }}
-        >
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+        <Container>
+            <Row className="justify-content-md-center">
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Row>
+        </Container>
     );
 }
 export default Jason
--- src/Servers.js
+++ src/Servers.js
@@ -2,19 +2,25 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 function Servers() {
     return (
         <>
-            <Alert variant="primary">
-                <Alert.Heading>Did you know we host a TF2 Server?</Alert.Heading>
-                <p>
-                Click below to join in all the fun. We have no random crits, a large pool of fun filled maps, and a cool server host!
-                </p>
-                <hr />
-                <div className="d-flex justify-content-end">
-                </div>
-            <Button href="steam://connect/prohunter69trades.com:27015">Connect</Button>
-            </Alert>
+            <Container fluid>                
+                <Row>
+                    <Alert variant="primary">
+                        <Alert.Heading>Did you know we host a TF2 Server?</Alert.Heading>
+                        <p>
+                        Click below to join in all the fun. We have no random crits, a large pool of fun filled maps, and a cool server host!
+                        </p>
+                        <hr />
+                        <div className="d-flex justify-content-end">
+                        </div>
+                    <Button href="steam://connect/prohunter69trades.com:27015">Connect</Button>
+                    </Alert>
+                </Row>
+            </Container>
         </>
     );
 }
